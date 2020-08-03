d409260030fa6e97da82f1f3f4114d89fd5d494a
Greyson Mascarenhas <greysonmrx@gmail.com>
2020-08-03 18:27:52 -0300

    feat: create home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { FiLogIn } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import './styles.css';
+
+const Home: React.FC = () => {
+  return (
+    <div id="page-home">
+      <div className="content">
+        <header>
+          <img src={logoImg} alt="Ecoleta" />
+        </header>
+
+        <main>
+          <h1>Seu marketplace de coleta de resíduos.</h1>
+          <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficient.</p>
+
+          <a href="/create-point">
+            <span>
+              <FiLogIn />
+            </span>
+            <strong>Cadastre um ponto de coleta</strong>
+          </a>
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
